25efe123b3fb93faf21ebdf232915a320c100760
Gabriel <gabrielvinicius4230@gmail.com>
2026-03-27 19:38:24 -0300

    fix: atualiza base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/guardio-banco-seguro/",
+  base: "/dbshield/",
   server: {
     host: "::",
     port: 8080,
